2f47b36364aecfef2bd6024d3bc455a8ddf84524
ncrqnt <github@ncrqnt.ch>
2021-10-11 13:10:53 +0200

    added enable/disable test

--- sigmadb/Public/Disable-SigmaRule.Tests.ps1
+++ sigmadb/Public/Disable-SigmaRule.Tests.ps1
@@ -1,4 +1,4 @@
-BeforeAll {
+BeforeDiscovery {
     #region Load mmodule
     $modulePath = "$PSScriptRoot\..\sigmadb.psm1"
     $moduleName = (($modulePath | Split-Path -Leaf) -replace '.psm1')
@@ -7,13 +7,24 @@ BeforeAll {
     #endregion
 }
 
-Describe "Disable-SigmaRule" {
-    BeforeEach {
-        Import-SigmaRule -
-    }
-    Context "Functionality Test" {
-        It "should disable sigma rule" {
+InModuleScope sigmadb {
+    Describe "Disable-SigmaRule" {
+        BeforeEach {
+            $config = "$TestDrive\sigmadb\config.json"
+            Import-SigmaRule -Path ".\testing\rules" -Config $config -NoProgressBar
+        }
+        Context "Functionality Test" {
+            It "should disable sigma rule" {
+                $id = '4976aa50-8f41-45c6-8b15-ab3fc10e79ed'
+                Disable-SigmaRule -Id $id -Config $config
 
+                $cfg = Get-PrivSigmaConfig -Config $Config
+                $db = New-Object -TypeName SigmaDB -ArgumentList $cfg.Files.Database
+                $rule = $db.Query("SELECT * FROM rule WHERE id = @id", @{ id = $id })[0]
+                $db.Close()
+
+                $rule.is_enabled | Should -Be '0'
+            }
         }
     }
 }
--- sigmadb/Public/Enable-SigmaRule.Tests.ps1
+++ sigmadb/Public/Enable-SigmaRule.Tests.ps1
@@ -1,4 +1,4 @@
-BeforeAll {
+BeforeDiscovery {
     #region Load mmodule
     $modulePath = "$PSScriptRoot\..\sigmadb.psm1"
     $moduleName = (($modulePath | Split-Path -Leaf) -replace '.psm1')
@@ -7,3 +7,24 @@ BeforeAll {
     #endregion
 }
 
+InModuleScope sigmadb {
+    Describe "Enable-SigmaRule" {
+        BeforeEach {
+            $config = "$TestDrive\sigmadb\config.json"
+            Import-SigmaRule -Path ".\testing\rules" -Config $config -Disable -NoProgressBar
+        }
+        Context "Functionality Test" {
+            It "should enable sigma rule" {
+                $id = '4976aa50-8f41-45c6-8b15-ab3fc10e79ed'
+                Enable-SigmaRule -Id $id -Config $config
+
+                $cfg = Get-PrivSigmaConfig -Config $Config
+                $db = New-Object -TypeName SigmaDB -ArgumentList $cfg.Files.Database
+                $rule = $db.Query("SELECT * FROM rule WHERE id = @id", @{ id = $id })[0]
+                $db.Close()
+
+                $rule.is_enabled | Should -Be '1'
+            }
+        }
+    }
+}
